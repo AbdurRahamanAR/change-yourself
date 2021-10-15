83db09975f63c7c218d5d85908757433f9a26c7b
Abdur Rahaman <abdur@hashdhronas.con>
2021-10-15 20:08:29 +0600

    save

--- Components/HabitProvider.test.js
+++
@@ -1,8 +0,0 @@
-import moment from 'moment';
-import {getLastFrequencyDate} from './HabitProvider';
-
-test('getLastFrequencyDate give correct date', () => {
-  expect(getLastFrequencyDate([0, 1, 2, 3, 4, 5, 6]).format('DD-MM-YYYY')).toBe(
-    moment().subtract(1, 'day').format('DD-MM-YYYY'),
-  );
-});
